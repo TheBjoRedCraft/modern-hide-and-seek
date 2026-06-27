f630d964c2a3b3116c001db5f1dbb488e76d24a4
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-06-27 15:36:07 +0200

    chore: update fabric.mod.json and FabricMain for client initialization

--- modern-hide-and-seek-fabric/src/main/kotlin/dev/slne/surf/hns/fabric/FabricMain.kt
+++ modern-hide-and-seek-fabric/src/main/kotlin/dev/slne/surf/hns/fabric/FabricMain.kt
@@ -1,10 +1,9 @@
 package dev.slne.surf.hns.fabric
 
 import net.fabricmc.api.ClientModInitializer
-import net.fabricmc.api.ModInitializer
 
 class FabricMain : ClientModInitializer {
     override fun onInitializeClient() {
-        TODO("Not yet implemented")
+
     }
 }

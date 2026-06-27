897c13039c6bcc1662842a2b91205d0f4b948c59
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-06-27 15:20:11 +0200

    chore: update FabricMain to implement ClientModInitializer

--- modern-hide-and-seek-fabric/src/main/kotlin/dev/slne/surf/hns/fabric/FabricMain.kt
+++ modern-hide-and-seek-fabric/src/main/kotlin/dev/slne/surf/hns/fabric/FabricMain.kt
@@ -1,9 +1,10 @@
 package dev.slne.surf.hns.fabric
 
+import net.fabricmc.api.ClientModInitializer
 import net.fabricmc.api.ModInitializer
 
-class FabricMain : ModInitializer {
-
-    override fun onInitialize() {
+class FabricMain : ClientModInitializer {
+    override fun onInitializeClient() {
+        TODO("Not yet implemented")
     }
 }

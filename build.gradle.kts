0f6839b34f010da81be6953250cf066cf2aea67e
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-06-27 13:48:39 +0200

    chore: gradle project

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+buildscript {
+    repositories {
+        gradlePluginPortal()
+        maven("https://reposilite.slne.dev/releases")
+    }
+    dependencies {
+        classpath("dev.slne.surf.api:surf-api-gradle-plugin:+")
+    }
+}
+
+allprojects {
+    group = "dev.slne.surf"
+    version = findProperty("version") as String
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://reposilite.slne.dev/releases")
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+    id("dev.slne.surf.api.gradle.settings") version "+"
+}
